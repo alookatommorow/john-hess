aa3ba6c6d3fb17c1dda4cca2c80e35d3b0e04681
alookatommorow <hessjohnt@gmail.com>
2016-03-14 14:50:33 -0700

    add initial controller, route, server

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-var express = require('express');
+var express = require('./config/express');
+
+
 var app = express();
-
-app.use('/', function(req, res){
-  res.send('Hello idiots');
-});
-
 app.listen(3000);
-console.log("server sucking balls at localhost:3000");
-
 module.exports = app;
+
+console.log("server sucking balls at localhost:3000");
