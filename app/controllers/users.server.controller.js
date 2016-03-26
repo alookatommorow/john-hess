5c47afe1656cd4b8ca65edfcd78e07592f753ec8
alookatommorow <hessjohnt@gmail.com>
2016-03-26 11:50:50 -0700

    add create user form, get /users

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,5 +1,15 @@
 var User = require('mongoose').model('User');
 
+exports.list = function(req, res, next) {
+  User.find({}, function(err, users){
+    if (err) {
+      return next(err);
+    } else {
+      res.json(users)
+    }
+  });
+};
+
 exports.create = function(req, res, next) {
   var user = new User(req.body);
   user.save(function(err) {
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,5 +1,9 @@
 var users = require('../../app/controllers/users.server.controller');
 
+
+
 module.exports = function(app) {
-  app.route('/users').post(users.create);
+  app.route('/users')
+    .get(users.list)
+    .post(users.create);
 };
